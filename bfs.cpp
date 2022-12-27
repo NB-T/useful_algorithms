dbba706a85517ef5a92c09ed379cc9cf6839c35c
NB-T <113719706+NB-T@users.noreply.github.com>
2022-12-27 15:01:18 -0500

    Add breadth-first–search

---
+++ bfs.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <vector>
+#include <Tree.h>
+
+using namespace std;
+
+void bfs(TreeNode* root)
+{
+     vector<TreeNode*> current;
+     vector<TreeNode*> next;
+     if (root) current.push_back(root);
+     while (current.size() > 0)
+     {
+          for (TreeNode* n : current)
+          {
+               // do something
+               if (n->left) next.push_back(n->left);
+               if (n->right) next.push_back(n->right);
+          }
+          current = next;
+          next.clear();
+     }
+}
